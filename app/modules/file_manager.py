110c355e0c665996b3b1ab4fd6bf78a503d24f05
greatbrain <erickfania@gmail.com>
2023-11-01 00:23:54 -0400

    Document reading module.

--- app/modules/file_manager.py
+++ app/modules/file_manager.py
@@ -2,15 +2,16 @@ import docx
 
 class File_Manager:          
       
-      async def open_document(self, document):
+      def read_document(self, docx_file):
           try:           
-            document = docx.Document(self.file)             
-            doc_text = [doc_text.append(paragraph.text) for paragraph in document.paragraphs]
-            self.fill_fields(doc_text)
+            document_object = docx.Document(docx_file)             
+            file_data= [paragraph.text for paragraph in document_object.paragraphs]
+            return file_data
+            # self.fill_fields(doc_text)
 
           except Exception as e:  
             return ("Something is wrong:", e)  
           
 
-      async def optimize_img(self, img):          
+      def optimize_img(self, img):          
           pass    
--- app/modules/validator.py
+++ app/modules/validator.py
@@ -3,20 +3,22 @@ from  modules.file_manager import File_Manager
 class Validator:
       def __init__(self):
           self.IMAGE_FORMATS = [".jpg", ".jpeg", ".png", ".webp", ".tiff", ".gif"]
-          self.manager = File_Manager()
-          #self.file_full_path = file_full_path 
-          #self.file_name_splited = file_name         
+          self.manager = File_Manager()   
       
 
-      def validate(self, file_full_path, file_name_splited=tuple):          
-          
-          if file_name_splited[1] == '.docx':                           
-             #self.manager.open_document(file_full_path)
-             print("Halo")
+      def validate(self, file_full_path, file_name_splited=tuple):  
 
-          elif file_name_splited[1] in self.IMAGE_FORMATS:             
-             #self.manager.optimize_img(file_full_path)
-             print("Halo-2")
+         try:
 
-          return "Extension de archivo incorrecta."
+            if file_name_splited[1] == '.docx':                           
+               print(self.manager.read_document(file_full_path))
+
+            elif file_name_splited[1] in self.IMAGE_FORMATS:             
+               #self.manager.optimize_img(file_full_path)
+               print("Halo-2")
+
+            return "Just docx and image format accepted"
+         
+         except Exception as e:           
+            raise ("Wrong file, try again.". e)
       
