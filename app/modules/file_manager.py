d6ebf7465ca35a55e057b029c9aad61c3370d580
greatbrain <erickfania@gmail.com>
2023-11-02 00:23:49 -0400

    Redefinition of the file

--- app/modules/file_manager.py
+++ app/modules/file_manager.py
@@ -2,21 +2,18 @@ import docx
 
 class File_Manager:          
       
-      def __init__(self, data_file) -> None:
-          self.data_file = data_file
+      def __init__(self) -> None:
+          self.data_file = None
 
-      def read_document(self):
+      def read_document(self, _file):
           try:           
-            document_object = docx.Document(self.data_file)             
-            docx_file_text = [paragraph.text for paragraph in document_object.paragraphs]
-            return docx_file_text            
+            document_object = docx.Document(_file)             
+            self.data_file = [paragraph.text for paragraph in document_object.paragraphs]
+            return self.data_file          
 
           except Exception as e:  
             return ("Something is wrong:", e)          
-      
-      def _cache_data(self):
-          pass
-          
-
+     
+     
       def optimize_img(self, img):          
           pass    
--- app/modules/validator.py
+++ app/modules/validator.py
@@ -3,16 +3,18 @@ from  modules.file_manager import File_Manager
 class Validator:
       def __init__(self):
           self.IMAGE_FORMATS = [".jpg", ".jpeg", ".png", ".webp", ".tiff", ".gif"]      
+          self.manager = File_Manager() 
 
       def validate(self, file_full_path, file_name_splited=tuple):  
 
          try:
 
             if file_name_splited[1] == '.docx':  
-               self.manager = File_Manager(file_full_path)                                           
+               print(self.manager.read_document(file_full_path))
 
             elif file_name_splited[1] in self.IMAGE_FORMATS:             
-               self.manager = File_Manager(file_full_path)
+               self.manager.optimize_img()
+
             return "Just docx and image format accepted"
          
          except Exception as e:           
