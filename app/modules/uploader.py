c8bf8ae4592d3ab9a88b01915763957aeb548123
greatbrain <erickfania@gmail.com>
2023-12-22 16:42:36 -0400

    Feat: Separating text by blocks

--- app/modules/uploader.py
+++ app/modules/uploader.py
@@ -4,20 +4,17 @@ from file_manager import File_Manager as FM
 class Uploader:
       def __init__(self):
           self.browser_con = mechsoup.Browser()                
-          self.URL = "https://gid.do/wp-admin/post-new.php"    
-
+          self.URL = "https://gid.do/wp-admin/post-new.php"
 
       ''' Connecting with browser and getting web html'''
       def get_page_body(self):          
           web_page = self.browser_con.get(self.URL)
           page_html = web_page.soup
 
-          return page_html     
-
+          return page_html
       
       def fill_text_fields(self, doc_text):
           form = self.get_web_page.select("")[0]          
-
 
       # We just upload the image, there's a Tiny plugin installed in the site that optimizes the images automatically.
       def upload_image(self):
--- app/modules/validator.py
+++ app/modules/validator.py
@@ -1,26 +1,28 @@
 ''' Identifies what kind of file has been found, image or .docx.'''
 
-from  modules.file_manager import File_Manager
+from modules.file_manager import File_Manager
+from modules.text_tager import Tager
 
 class Validator:
       def __init__(self):
           self.IMAGE_FORMATS = [".jpg", ".jpeg", ".png", ".webp", ".tiff", ".gif"]      
-          self.manager = File_Manager() 
-
+          self.manager = File_Manager()
 
       def validate_files(self, file_full_path, file_name_splited=tuple):  
 
          try:
 
             if file_name_splited[1] == '.docx':                 
-               self.manager.set_data_docuement(file_full_path)
+               self.manager.set_data_document(file_full_path)
 
                #Just for trial purposes
-               for i in self.manager.get_data_document():
-                   print(i)
+               '''for i in self.manager.get_data_document():
+                   print(i)'''
+               
+               self.manager.get_data_document()
 
             elif file_name_splited[1] in self.IMAGE_FORMATS:                            
-               self.manager.set_image_file(file_full_path)                
+               self.manager.set_image_file(file_full_path)     
 
             return "Just .docx or an image format is allowed."
          
