17eeb2a6c9af133838459cdbbf10c0c056c8748f
greatbrain <erickfania@gmail.com>
2023-12-28 14:39:12 -0400

    refactor:using built-in func __call__

--- app/modules/formatter.py
+++ app/modules/formatter.py
@@ -4,17 +4,17 @@ import docx
 class Formatter:
 
       def __init__(self, blog_entry_file_text, blog_entry_image):
-          
           self._text_file = blog_entry_file_text
           self._image_file = blog_entry_image
 
-          self.json_response = {'category':'', 
-                             'title':'',
-                             'summary':'',                              
-                             'body':'', 
-                             'tags':'',
-                             'image_url':''
-                            }
+          self.json_response = {
+                  'category':'', 
+                  'title':'',
+                  'summary':'',                              
+                  'body':'', 
+                  'tags':'',
+                  'image_url':''
+              }
           
           self.TEXT_STYLES = {         
                   'Heading 3':'Category',         
@@ -23,26 +23,23 @@ class Formatter:
                   'Body Text':'Body',
                   'Heading 5':'Tags',
                   'Heading 6':'Tags',
-               } 
-
-      def run(self):
-          pass                    
+               }
       
-      def get_text(self):
-          docx_document_object = docx.Document(self._text_file)    
-          return docx_document_object.paragraphs
+      def _get_text(self):          
+          return docx.Document(self._text_file).paragraphs
       
-      
-      #Filter text by sections and returns it as a generator.
-      def classify_text(self):                          
-          for item in self.get_text():              
-              text_style = item.style.name
-              block_text = self.TEXT_STYLES.get(text_style, 'Default Paragraph Style')
-              _text_dict = {block_text:item.text}              
-              yield _text_dict
-              
+      #Filtering text by blocks.
+      def _classify_text(self):   
+          yield list(map(lambda item : {self.TEXT_STYLES.get(item.style.name, 'Default Paragraph Style'): item.text}, self._get_text()))         
       
       def get_response(self):                    
-           for i in self.classify_text():
-               print(i) 
+          for index, item in enumerate(self._classify_text()):
+              print(item)
+      
+      def run(self):
+          self.get_response()
+        
+      
+      __call__ = run
+      #To be executed from the Validator class.
         
--- app/modules/validator.py
+++ app/modules/validator.py
@@ -1,14 +1,14 @@
-''' Identifies what kind of file has been found, image or .docx.'''
 from modules.formatter import Formatter
 
 class Validator:
       def __init__(self):
           self.blog_entry_file_text = None
           self.blog_entry_image = None
-          self.IMAGE_FORMATS = [".jpg", ".jpeg", ".png", ".webp", ".tiff", ".gif"] 
 
-
-      #To know if files encountered are we are expecting for:
+          self.IMAGE_FORMATS = [".jpg", ".jpeg", ".JPG", ".JPEG", 
+                                ".png", ".PNG", ".webp", ".tiff", ".gif"
+                                ]
+      
       def validate_files(self, file_full_path, file_name_splited=tuple):
           try:               
              for ext in file_name_splited:
@@ -16,11 +16,13 @@ class Validator:
                     self.blog_entry_file_text = file_full_path
 
                  elif ext in self.IMAGE_FORMATS:
-                      self.blog_entry_image = file_full_path                       
-            
-             self.formatter = Formatter(self.blog_entry_file_text, self.blog_entry_image)
-             #self.formatter.classify_text()
-             self.formatter.get_response()
+                      self.blog_entry_image = file_full_path
+             
+             self.formatter = Formatter(self.blog_entry_file_text, self.blog_entry_image)             
+             
+             """Running the 'Formatter' class as a function,
+             using the __call__ built-in method"""
+             self.formatter()             
          
           except FileNotFoundError as e:           
             raise ("Wrong file, try again.", e)
