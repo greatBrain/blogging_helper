79a3e1230850116011b807c2eed538625756549c
greatbrain <erickfania@gmail.com>
2024-02-14 15:50:54 -0400

    Add: Main starting window

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from modules import observer
 
-if __name__=='__main__':
-   observer_instance = observer.Main()   
+class Main:
+      def __init__(self) -> None:
+          self.observer_instance = observer.Main()   
 
-   try:            
-      observer_instance.run()
-                     
-   except RuntimeError as e:
-      raise("Failed to run the program.", e)
+      def run(self):
+         try:            
+            self.observer_instance.run()
+         except RuntimeError as e:
+            raise("Failed to run the program.", e)
--- gui/main_window.py
+++ gui/main_window.py
@@ -2,6 +2,9 @@ import tkinter
 import customtkinter
 import os
 from PIL import ImageTk, Image
+import app.main as main
+
+#### IMPORTING APP MODULE NOT WORKING ####
 
 class MainWindow:
       def __init__(self) -> None:  
@@ -12,14 +15,18 @@ class MainWindow:
            self.theme = customtkinter.set_default_color_theme("green")
            self.button = object
 
+           #Images in button
+           self.turn_off_image = customtkinter.CTkImage(Image.open(os.path.join(self.imagesDir, "turn_off_icon.ico")), size=(155, 155))
+           self.turn_on_image = customtkinter.CTkImage(Image.open(os.path.join(self.imagesDir, "turn_on_icon.ico")), size=(155, 155))
+
       def create_window(self):  
           self.app.title("")  
           #Linux doesn't accept .ico as icon:
           icon = ImageTk.PhotoImage(Image.open(os.path.join(self.imagesDir, "robotic-arm-logo.png")))  
           self.app.wm_iconphoto(True, icon)             
-          self.app.geometry("550x400")
-          title_label = customtkinter.CTkLabel(master=self.app, text="Auto Blogger", font=("Arial", 23))
-          title_label.pack(pady = 50)
+          self.app.geometry("550x400")          
+          title_label = customtkinter.CTkLabel(master=self.app, text="Blogging Ayudante", font=("sans", 24))
+          title_label.pack(pady = 30)
           self.app.resizable(False, False)          
           self.create_button()
 
@@ -27,23 +34,29 @@ class MainWindow:
       def create_button(self):
           self.button = customtkinter.CTkButton(master=self.app, 
                                                 text="",                                                 
-                                                image= customtkinter.CTkImage(
-                                                    Image.open(os.path.join(self.imagesDir, "turn_off_icon.ico")),
-                                                    size=(125, 125)
-                                                ),     
+                                                image= self.turn_off_image,    
                                                 font=("Arial", 20),
                                                 border_spacing=(2),
                                                 hover=False,                                                
                                                 fg_color="transparent",
                                                 bg_color="transparent",
-                                                command=self.on_click
+                                                command=self.on_click                                                 
                                             )
           self.button.pack(pady=20, padx=20)
           self.button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
+    
+      def set_app_status(self, text):
+          app_status = text
+          return app_status
+
       def on_click(self):
-          if self.button._image=="turn_off_icon.ico":
-             print("SAluuudo")
+          if self.button._image==self.turn_off_image:
+             self.button.configure(image=self.turn_on_image)  
+             main.run()           
+             
+          else:
+             self.button.configure(image=self.turn_off_image)
 
       def run(self):    
           return self.app.mainloop()  
@@ -53,19 +66,3 @@ if __name__ == "__main__":
     root = MainWindow()
     root.create_window()
     root.run()
-
-
-
-''' 
-#frame_button = customtkinter.CTkFrame(master=self.app, corner_radius=10)
-#frame_button.pack(pady=30, padx=30, fill="both", expand=True)
-'''
-
-#Logo image:
-'''logo = customtkinter.CTkImage(
-           Image.open(os.path.join(self.imagesDir, "robotic-arm-logo.png")),
-           size=(150, 150)
-          )'''
-#Label that contains the logo image
-'''logo_label = customtkinter.CTkLabel(master=self.app, image=logo, text="")
-logo_label.pack(pady = 50)'''
